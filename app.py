b257bb9a9aaa6b1bf3a50071d9358e607afbeace
Aayush raj thakur <aayushrajthakur@gmail.com>
2026-02-01 12:49:55 +0530

    Again table variables sizechanges

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ db = SQLAlchemy(app)
 
 class Users(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(15), unique=True, nullable=False)
-    email = db.Column(db.String(15), nullable=False)
-    hashed_password = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    hashed_password = db.Column(db.String(120), nullable=False)
 
 # Create database tables if they don't exist
 with app.app_context():
